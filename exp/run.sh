adf80cbd51a53279914c70e25e8f57335e104802
Sean Smith <seanwssmith@gmail.com>
2016-04-21 17:51:35 +0800

    added the exp folder.

---
+++ exp/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#/usr/local/mpi3/bin/mpirun -l nodes=8:ppn=64  ./train.sh
+
+#/usr/local/mpi3/bin/mpiexec  ./train.sh
---
+++ exp/train.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# for workload1
+./dnntk dnn_config_debug.cfg
+# for workload2
+#./dnntk dnn_config_tr.cfg
